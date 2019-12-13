d1f36b7939740d1d6a61cf4176fd13173dde45ca
aunurDev <develop.aunur@gmail.com>
2019-12-13 11:28:28 -0800

    fix size hover bg

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,33 @@
 // hover background
 $(document).ready(function(){
+	// option1
+	// if ($("#link-gedung").mouseenter()) {
+	// 	$(".push-right").css("display", "block");
+	// 	$("#content").css("display", "none");
+	// }else{
+	// 	$(".push-right").css("display", "none");
+	// 	$("#content").css("display", "block");
+	// }
+
+	// option2
+	// $('#link-gedung').mouseenter(function(){
+	// 	$(".push-right").css("display", "block");
+	// 	$("#content").css("display", "none");
+	// 	$(this).mouseleave(function(){
+	// 		$(".push-right").css("display", "none");
+	// 		$("#content").css("display", "block");
+	// 	});
+	// });
+
+	// option3
 	$('#link-gedung').mouseenter(function(){
-		$(".push-right").css("display", "block");
+		if ($(".push-right").css("display", "block")) {
+			$("#content").css("display", "none");	
+		}
 		$(this).mouseleave(function(){
-			$(".push-right").css("display", "none");
+			if ($(".push-right").css("display", "none")) {
+				$("#content").css("display", "block");
+			}	
 		});
 	});
 });
