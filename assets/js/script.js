50983d2f35d996a9a0bef73ac6bce9bfc3f10c50
aunurDev <develop.aunur@gmail.com>
2019-12-23 20:50:36 -0800

    hover background

--- assets/js/script.js
+++ assets/js/script.js
@@ -65,7 +65,84 @@ $(document).ready(function(){
 		});
 	});
 
-	var menu = new cbpTooltipMenu( document.getElementById( 'cbp-tm-menu' ) );
+	$('#link-produk-andalan').mouseenter(function(){
+		if ($("#andalan.push-right").css("display", "block")) {
+			$("#content").css("display", "none");	
+		}
+		$(this).mouseleave(function(){
+			if ($(".push-right").css("display", "none")) {
+				$("#content").css("display", "block");
+			}	
+		});
+	});
+
+	$('#link-visimisi').mouseenter(function(){
+		if ($("#visimisi.push-right").css("display", "block")) {
+			$("#content").css("display", "none");	
+		}
+		$(this).mouseleave(function(){
+			if ($(".push-right").css("display", "none")) {
+				$("#content").css("display", "block");
+			}	
+		});
+	});
+
+	$('#link-finansial').mouseenter(function(){
+		if ($("#finansial.push-right").css("display", "block")) {
+			$("#content").css("display", "none");	
+		}
+		$(this).mouseleave(function(){
+			if ($(".push-right").css("display", "none")) {
+				$("#content").css("display", "block");
+			}	
+		});
+	});
+
+	$('#link-sejarah').mouseenter(function(){
+		if ($("#sejarah.push-right").css("display", "block")) {
+			$("#content").css("display", "none");	
+		}
+		$(this).mouseleave(function(){
+			if ($(".push-right").css("display", "none")) {
+				$("#content").css("display", "block");
+			}	
+		});
+	});
+
+	$('#link-pemasaran').mouseenter(function(){
+		if ($("#pemasaran.push-right").css("display", "block")) {
+			$("#content").css("display", "none");	
+		}
+		$(this).mouseleave(function(){
+			if ($(".push-right").css("display", "none")) {
+				$("#content").css("display", "block");
+			}	
+		});
+	});
+
+	$('#link-sertifikasi').mouseenter(function(){
+		if ($("#sertifikasi.push-right").css("display", "block")) {
+			$("#content").css("display", "none");	
+		}
+		$(this).mouseleave(function(){
+			if ($(".push-right").css("display", "none")) {
+				$("#content").css("display", "block");
+			}	
+		});
+	});
+
+	$('#link-peduli').mouseenter(function(){
+		if ($("#peduli.push-right").css("display", "block")) {
+			$("#content").css("display", "none");	
+		}
+		$(this).mouseleave(function(){
+			if ($(".push-right").css("display", "none")) {
+				$("#content").css("display", "block");
+			}	
+		});
+	});
+
+
 }); // end hoover background
 
 
